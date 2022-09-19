6ee0c5e24ae472e8a4076699e2a2cb5d41cffcf9
Vinit Suryarao <suryarao.vinit@hotmail.com>
2022-09-19 21:20:31 +0530

    First Commit

---
+++ ClientApplication/HomePage.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="HomePage.aspx.cs" Inherits="ClientApplication.HomePage" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:TextBox ID="txtNumber1" runat="server" ></asp:TextBox>  &nbsp;&nbsp;
+            <asp:TextBox ID="txtNumber2" runat="server" ></asp:TextBox></br>
+            <asp:Button ID="btnAdd" runat="server" Text="Add" OnClick="btnAdd_Click" /> &nbsp;&nbsp;
+            <asp:Button ID="btnSub" runat="server" Text="Sub" OnClick="btnSub_Click" /></br>
+            <asp:Label ID="lblResult" runat="server" ></asp:Label>
+        </div>
+        
+    </form>
+</body>
+</html>
---
+++ ClientApplication/HomePage.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ClientApplication
+{
+    public partial class HomePage : System.Web.UI.Page
+    {
+        // Creatig object of service client reference
+        MathematicServiceReference.MathematicServiceClient objServiceClient = MathematicServiceReference.MathematicServiceClient();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            // Create object of Data Contract. The Structure wich communicate service
+            MathematicServiceReference.MyNumber objMyNumber = new MathematicServiceReference.MyNumber();
+
+            objMyNumber.Number1 = Convert.ToInt32(txtNumber1.Text);
+            objMyNumber.Number2 = Convert.ToInt32(txtNumber2.Text);
+
+            // Call Service Method
+            int result = objServiceClient.Add(objMyNumber);
+
+            lblResult.Text = "Addtion = " + result.ToString();
+        }
+
+        protected void btnSub_Click(object sender, EventArgs e)
+        {
+            // Create object of Data Contract. The Structure wich communicate service
+            MathematicServiceReference.MyNumber objMyNumber = new MathematicServiceReference.MyNumber();
+
+            objMyNumber.Number1 = Convert.ToInt32(txtNumber1.Text);
+            objMyNumber.Number2 = Convert.ToInt32(txtNumber2.Text);
+
+            // Call Service Method
+            int result = objServiceClient.Sub(objMyNumber);
+
+            lblResult.Text = "Subtraction = " + result.ToString();
+        }
+    }
+}
